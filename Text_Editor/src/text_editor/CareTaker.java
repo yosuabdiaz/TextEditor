42da08befae835c28aa904d15d3fe52f8970be17
MrLuis137 <lalemn2@gmail.com>
2021-11-02 11:01:06 -0600

    Agregado máximo de 20 mementos

--- Text_Editor/src/text_editor/CareTaker.java
+++ Text_Editor/src/text_editor/CareTaker.java
@@ -10,19 +10,27 @@ package text_editor;
  * @author lalem
  */
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CareTaker {
-    ArrayList<Memento> memento = new ArrayList<>();
+    //ArrayList<Memento> memento = new ArrayList<>();
+    Queue<Memento> memento = new LinkedList<Memento>();
     int currentIndex;
     
     void addMemento(Memento memento){
-        if(currentIndex >= this.memento.size())
+        if(currentIndex == 20){
+            this.memento.remove();
+            this.memento.add(memento);
+            return;
+        }
         this.memento.add(memento);
         currentIndex++;
     } 
     
     Memento getMemento(int index){
-        return (index >= memento.size())? null : memento.get(index);
+        return (index >= memento.size())? null : 
+                ((LinkedList<Memento>)memento).get(index);
             
     }
     
@@ -31,7 +39,7 @@ public class CareTaker {
             return null;
         }
         currentIndex++;
-        return memento.get(currentIndex);
+        return ((LinkedList<Memento>)memento).get(currentIndex);
     }
     
     Memento getPreviousState(){
@@ -41,7 +49,7 @@ public class CareTaker {
         }
         currentIndex--;
         System.out.println(currentIndex -1);
-        return memento.get(currentIndex -1);
+        return ((LinkedList<Memento>)memento).get(currentIndex -1);
     }
     
 }
