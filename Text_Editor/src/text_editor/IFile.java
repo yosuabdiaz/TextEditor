4768adff6d4a42c2a5f85de103cc4f74694fb651
Yosua Blanco <yosuabd@estudiantec.cr>
2021-11-04 16:35:18 -0600

    Esqueleto de Abrir y Cerrar

---
+++ Text_Editor/src/text_editor/IFile.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text_editor;
+
+/**
+ *
+ * @author Yosua Blanco Diaz
+ */
+interface IFile {
+    void loadFile();
+    void saveFile();
+}
---
+++ Text_Editor/src/text_editor/TXT.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text_editor;
+
+/**
+ *
+ * @author Yosua Blanco Diaz
+ */
+public class TXT implements IFile{
+
+    @Override
+    public void loadFile() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void saveFile() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
