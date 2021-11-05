63ce70017b696f68166f40eb62d3fdbda275f7ca
MrLuis137 <lalemn2@gmail.com>
2021-11-05 01:44:20 -0600

    GetBackgroundColors

--- Text_Editor/src/utils/StyledDocumentManager.java
+++ Text_Editor/src/utils/StyledDocumentManager.java
@@ -5,6 +5,12 @@
  */
 package utils;
 
+import java.awt.Color;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.StyledDocument;
 
 /**
@@ -13,7 +19,31 @@ import javax.swing.text.StyledDocument;
  */
 public class StyledDocumentManager {
     
-   static public int[] getBackgroundColors(){
+   static public int[] getBackgroundColors(StyledDocument doc){
+        String text = "";
+        
+        try {
+            text = doc.getText(0, doc.getLength());
+        } catch (BadLocationException ex) {
+            Logger.getLogger(StyledDocumentManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        int[] colors = new int[text.length()];
+        
+        for(int i = 0; i< text.length(); i++){
+            AttributeSet chatacterAtributes = doc.getCharacterElement(i).getAttributes();
+            Enumeration<?> atributes = chatacterAtributes.getAttributeNames();
+            System.out.println("Char: " + text.charAt(i));
+            int colorNumber = 0;
+            while(atributes.hasMoreElements()){
+                Object s = chatacterAtributes.getAttribute(atributes.nextElement());
+                if(s.getClass() == Color.class){
+                    Color color = (Color)s;
+                    colorNumber = color.getRGB();
+                }
+            }
+            colors[i] = colorNumber;
+            
+        }
     return null;
    }
    
